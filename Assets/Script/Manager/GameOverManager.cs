f334eefb8da2935df2130c7ad5b5132e69405e39
DaffaPutra7 <Putradika3604@gmail.com>
2026-01-08 22:04:50 +0700

    Membuat script GameOverManager dan melakukan perubahan pada script EnemyController

---
+++ Assets/Script/Manager/GameOverManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GameOverManager : MonoBehaviour
+{
+    public GameObject gameOverText; 
+
+    public void TriggerGameOver()
+    {
+        gameOverText.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
